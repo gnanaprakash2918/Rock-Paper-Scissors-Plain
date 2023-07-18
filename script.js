01aae586710a5811dea546edb8a3f4f335139d4c
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-07-18 20:55:22 +0530

    Copied changes

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
-
-function getPlayerChoice() {}
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
@@ -34,4 +32,77 @@ function playRound(playerSelection, computerSelection) {
       return -1;
     }
   }
+}
+
+let playerScore = 0,
+  computerScore = 0,
+  ties = 0,
+  rounds = 0;
+
+const divSelector = document.getElementsByClassName("image-container");
+const arr = Array.from(divSelector);
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i].addEventListener("click", function () {
+    let playerChose = arr[i].className.split(" ")[1];
+    let computerChose = getComputerChoice();
+
+    document.querySelector(
+      "body > div.scores-container.winner > div > div.player > span.player-score.winner"
+    ).textContent = playerChose;
+
+    document.querySelector(
+      "body > div.scores-container.winner > div > div.computer > span.computer-score.winner"
+    ).textContent = computerChose;
+
+    let winner = playRound(playerChose, computerChose);
+    if (winner == 1) {
+      playerScore += 1;
+      document.querySelector(
+        "body > div:nth-child(3) > div > div.player > span.player-score"
+      ).textContent = playerScore;
+      if (ties) {
+        ties = 0;
+        const t = document.querySelector("body > div.scores-container.winner");
+        t.removeChild(t.lastChild);
+      }
+    } else if (winner == 0) {
+      computerScore += 1;
+      document.querySelector(
+        "body > div:nth-child(3) > div > div.computer > span.computer-score"
+      ).textContent = computerScore;
+    } else {
+      ties += 1;
+      if (ties == 1) {
+        const sp = document.createElement("span");
+        sp.textContent = "Tie !";
+        sp.setAttribute("style", "text-align:center");
+        document
+          .querySelector("body > div.scores-container.winner ")
+          .appendChild(sp);
+      }
+    }
+
+    if (playerScore == 5 || computerScore == 5) {
+      let won = playerScore == 5 ? "You won" : "Computer won";
+      alert(`${won}`);
+
+      console.log(playerScore, computerScore);
+      document.querySelector(
+        "body > div.scores-container.winner > div > div.player > span.player-score.winner"
+      ).textContent = "";
+      document.querySelector(
+        "body > div.scores-container.winner > div > div.computer > span.computer-score.winner"
+      ).textContent = "";
+      document.querySelector(
+        "body > div:nth-child(3) > div > div.player > span.player-score"
+      ).textContent = 0;
+      document.querySelector(
+        "body > div:nth-child(3) > div > div.computer > span.computer-score"
+      ).textContent = 0;
+      playerScore = computerScore = 0;
+      playerChose = computerChose = "";
+    }
+  });
 }
